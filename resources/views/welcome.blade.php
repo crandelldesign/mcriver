fe1de4e056d2d4a0ec08a4838bf0902d9b1e6254
Crandell, Matt <mcrandell@jrthompson.com>
2016-01-22 10:00:45 -0500

    Updated some text.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">McRiver Raid {{date('Y')}}</div>
             </div>
         </div>
     </body>

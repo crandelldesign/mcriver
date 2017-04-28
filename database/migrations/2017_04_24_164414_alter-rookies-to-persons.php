89355a28f7a8dc8ca793466b67485b81e49cb15c
Crandell, Matt <mcrandell@jrthompson.com>
2017-04-28 08:19:08 -0400

    Added rookie boolean.

--- database/migrations/2017_04_24_164414_alter-rookies-to-persons.php
+++ database/migrations/2017_04_24_164414_alter-rookies-to-persons.php
@@ -14,6 +14,7 @@ class AlterRookiesToPersons extends Migration
     {
         Schema::rename('rookies', 'persons');
         Schema::table('persons', function ($table) {
+            $table->boolean('is_rookie')->default(0);
             $table->boolean('is_active')->default(1);
             $table->integer('order_id')->unsigned()->nullable();
             $table->foreign('order_id')->references('id')->on('orders');
@@ -34,6 +35,7 @@ class AlterRookiesToPersons extends Migration
             $table->dropColumn('is_active');
         });
         Schema::table('persons', function ($table) {
+            $table->dropColumn('is_rookie');
             $table->dropColumn('is_active');
             $table->dropForeign(['order_id']);
             $table->dropColumn('order_id');

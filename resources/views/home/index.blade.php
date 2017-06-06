9a0f6123a120c1afe0c5e98dad51f24e51fa9c36
Matt Crandell <mrcrandell@gmail.com>
2017-06-05 23:45:18 -0400

    Fixed year.

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.default')
 @section('content')
-<h1>McRiver Raid 2016</h1>
+<h1>McRiver Raid 2017</h1>
 
 <p class="text-center featured-img-container"><img src="{{url('/')}}/img/group-shot-2016.jpg" class="img-responsive center-block featured-img" alt="2016's Group"></p>
 
